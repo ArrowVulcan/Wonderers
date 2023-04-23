0bc35f57da799485638445300e7f9f6b1f1b1306
ArrowVulcan <stalkeer@hotmail.com>
2023-04-23 19:09:57 +0200

    Site remake
    Remade the entire site.

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+function close_menu(e){
+
+    let menu = document.getElementById(e)
+
+    let fade_time = 200
+
+    menu.style.transition = "opacity " + fade_time + "ms"
+    menu.style.opacity = 0
+
+    setTimeout(() => {
+        menu.style.display = "none"
+    }, fade_time);
+
+}
+
+function open_menu(e, keep=false){
+
+    if(!keep){
+
+        document.querySelectorAll(".menus").forEach(function(e){
+            e.style.display = "none"
+        })
+
+    }
+
+    let menu = document.getElementById(e)
+
+    let fade_time = 200
+
+    menu.style.transition = "opacity " + fade_time + "ms"
+
+    menu.style.display = "flex"
+
+    setTimeout(() => {
+        menu.style.opacity = 1
+    }, 1);
+
+}
